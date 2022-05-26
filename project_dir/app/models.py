f34318ffc76192b51bd03999ef4f608b312380f9
Bodybro <ivan.stereotekk@gmail.com>
2022-05-27 02:21:11 +0400

    model psycopg go to sleep !

--- project_dir/app/models.py
+++ project_dir/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Person(models.Model,User):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    email = models.EmailField(null=True)
+    login = models.TextField(max_length=100)
+    token = models.TextField(max_length=555)
+    class Meta:
+        uniq_together = ('email','login')
+
+
+class Post(models.Model):
+    title = models.TextField(null=True)
+    autor = models.ForeignKey(Person,on_delete=models.PROTECT)
+    discription = models.TextField(null=True)
+    content = models.TextField(max_length=3000)
+    req_count = models.IntegerField()
+    like = models.IntegerField()
+    disslike = models.IntegerField()
+    slag_field = models.CharField()
+    class Meta:
+        uniq_together = ('title','slag_field')
--- project_dir/app/urls.py
+++ project_dir/app/urls.py
@@ -1,8 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path,include
+from .views import*
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('app.urls')),
-]
+    path('index/',index),
+
+    ]
