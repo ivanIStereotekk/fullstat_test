cb54f2f602bf16e6b217c8dde4757486d590b3ee
Bodybro <ivan.stereotekk@gmail.com>
2022-06-08 02:33:12 +0400

    Corrected Serializers Link

--- project_dir/app/serializers.py
+++ project_dir/app/serializers.py
@@ -44,6 +44,7 @@ class Bookmark_Serializer(serializers.ModelSerializer):
 
 #-----
 class Link_Serializer(serializers.ModelSerializer):
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
     class Meta:
         model = Link
         fields = "__all__"
