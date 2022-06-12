265ee0a919dbc8e4b272243c16952b0d6122a640
Bodybro <ivan.stereotekk@gmail.com>
2022-06-12 12:23:18 +0400

    Log_Out Test Cases

--- project_dir/app/tests.py
+++ project_dir/app/tests.py
@@ -1,6 +1,6 @@
 #from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase #APIClient, APIRequestFactory, force_authenticate
+from rest_framework.test import APITestCase, APIClient, APIRequestFactory, force_authenticate
 #from app.models import Person,Post
 from app.views import *
 
@@ -32,16 +32,79 @@ class Anonimous_User_Tests_Cases(APITestCase):
         self.assertEqual(response.data["results"], [])
 
 
-# class Logged_User_Test_Cases(APITestCase):
-#     """
-#     The Pipeline of different test cases for registred user:
-#     """
+class Logg_Out_User_Test_Cases(APITestCase):
+    """
+    The Pipeline of different test cases for logged off user:
+    """
+    client = APIClient()
+    client.force_authenticate(user=None,token=None)
+    def test_get_my_posts(self):
+        url = 'http://127.0.0.1:8000/api/my_posts/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], "Authentication credentials were not provided.")
+    def test_my_bookmarks(self):
+        url = 'http://127.0.0.1:8000/api/my_bookmarks/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], "Authentication credentials were not provided.")
+    def test_my_reactions(self):
+        url = 'http://127.0.0.1:8000/api/my_reactions/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], "Authentication credentials were not provided.")
+    def test_get_token(self):
+        url = 'http://127.0.0.1:8000/api/token/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(response.data['detail'], "Method \"GET\" not allowed.")
+    def test_get_post(self):
+        url = 'http://127.0.0.1:8000/api/posts_model/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], "Authentication credentials were not provided.")
+    def test_get_bookmarks(self):
+        url = 'http://127.0.0.1:8000/api/bookmarks_model/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], "Authentication credentials were not provided.")
+    def test_get_reactions(self):
+        url = 'http://127.0.0.1:8000/api/reactions_model/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], "Authentication credentials were not provided.")
+    def test_get_users(self):
+        url = 'http://127.0.0.1:8000/auth/users/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], "Authentication credentials were not provided.")
+    def test_get_users_me(self):
+        url = 'http://127.0.0.1:8000/auth/users/me/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], "Authentication credentials were not provided.")
+    def test_get_login(self):
+        url = 'http://127.0.0.1:8000/auth/token/login/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(response.data['detail'], "Method \"GET\" not allowed.")
+    def test_get_logout(self):
+        url = 'http://127.0.0.1:8000/auth/token/logout/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], "Authentication credentials were not provided.")
 
 
 
-        # data = {'title': 'Title', 'discription': 'Discrition', "slug": "Test_Slug", 'content': 'Test-Content','author': None, "req_count": 0}
-        # response = self.client.post(url, data, format='json')
-        # self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+
+
+
+
+    # data = {'title': 'Title', 'discription': 'Discrition', "slug": "Test_Slug", 'content': 'Test-Content','author': None, "req_count": 0}
+    # response = self.client.post(url, data, format='json')
+    # self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
 #   data = {'title': 'Title', 'discription': 'Discrition', "slug": "Test_Slug", 'content': 'Test-Content',
