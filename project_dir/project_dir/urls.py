968184cf5a9d85539b13c10730d6b5a154609796
Bodybro <ivan.stereotekk@gmail.com>
2022-06-14 22:58:59 +0400

    Ran 39 tests in 2.062s
    OK

--- project_dir/project_dir/urls.py
+++ project_dir/project_dir/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.urls.authtoken')),
-    #path('auth/webauthn/', include('djoser.webauthn.urls')),
+    #path('auth/webauthn/', include('djoser.webauthn.urls')),      maybe next time...
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
